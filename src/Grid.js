03c36cfe7c3e46f26c43f4bf04cd83da2ea4c386
Ujala <ujala21@navgurukul.com>
2023-10-09 12:42:46 +0530

    Hidden

--- src/Grid.js
+++ src/Grid.js
@@ -1,15 +1,13 @@
 import React from "react";
-import {Grid} from "@material-ui/core"
+import {Grid,Hidden} from "@material-ui/core"
 const MyGrid=()=>{
     return(
         <>
             <Grid item container spacing={1}>
                 <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"blue"}}>Box1</h1></Grid>
-                <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"blue"}}>Box2</h1></Grid>
+                <Hidden only={["sm","xs"]}><h1 style={{backgroundColor:"blue"}}>Box 2</h1></Hidden>
                 <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"blue"}}>Box3</h1></Grid>
-                <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"blue"}}>Boxx3</h1></Grid>
-                
-
+                <Grid item lg={3} sm={6} xs={12}><h1 style={{backgroundColor:"blue"}}>Box4</h1></Grid>
             </Grid>
         </>
     )
