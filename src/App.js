66f6329cebfa61514c2b3fe9513b8f45c7066440
Ujala <ujala21@navgurukul.com>
2023-10-30 19:11:46 +0530

    material mui use Skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 // import Dialog1 from "./Dialog"
-import Progress1 from "./progress"
+// import Progress1 from "./progress"
+import Skeleton1 from "./Skeleton"
 const App=()=>{
   return(
     <>
         {/* <Dialog1/> */}
-        <Progress1/>
+        {/* <Progress1/> */}
+        <Skeleton1/>
     </>
   )
 }
