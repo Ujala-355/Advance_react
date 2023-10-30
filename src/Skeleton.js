8bcb3ca057640cde015b03d67b6734e3ace6e97e
Ujala <ujala21@navgurukul.com>
2023-10-30 19:32:38 +0530

    Skeleton

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -1,4 +1,4 @@
-import {Stack,Skeleton} from "@mui/material"
+import {Box,Stack,Skeleton} from "@mui/material"
 const Skeleton1=()=>{
     return(
         <>
@@ -7,6 +7,17 @@ const Skeleton1=()=>{
                 <Skeleton variant="circular" width={40} height={40}/>
                 <Skeleton variant="rectangular" width={250} height={125}/>
             </Stack>
+            <Box sx={{width:300}}>
+                <Skeleton/>
+                <Skeleton animation="wave"/>
+                <Skeleton animation={false}/>
+            </Box>
+            <Skeleton
+                sx={{bgcolor:"grey.900"}}
+                variant="rectangular"
+                width={210}
+                height={118}
+            />
         </>
     )
 }
